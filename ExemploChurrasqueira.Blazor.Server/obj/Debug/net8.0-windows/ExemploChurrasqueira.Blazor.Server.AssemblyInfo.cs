c08dcec44dd9842ad6eb4b0fdd69fded972ed1e2
GabrielValgas <126532663+Valgas491@users.noreply.github.com>
2025-02-26 09:43:11 -0300

    Log Delete

--- ExemploChurrasqueira.Blazor.Server/obj/Debug/net8.0-windows/ExemploChurrasqueira.Blazor.Server.AssemblyInfo.cs
+++ ExemploChurrasqueira.Blazor.Server/obj/Debug/net8.0-windows/ExemploChurrasqueira.Blazor.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExemploChurrasqueira.Blazor.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1234ca7ba8daf191410c824602cf067cc8956317")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e25e9db7f2f8206b086a845469dfb3e17c5655c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExemploChurrasqueira.Blazor.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExemploChurrasqueira.Blazor.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.*")]
